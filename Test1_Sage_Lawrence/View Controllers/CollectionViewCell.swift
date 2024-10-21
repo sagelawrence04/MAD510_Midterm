21f62d14090e77dcd4436e26dc95863dd7546f8d
Sage Lawrence <w0811501@myscc.ca>
2024-10-20 20:43:51 -0400

    Files Created

---
+++ Test1_Sage_Lawrence/View Controllers/CollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CollectionViewCell.swift
+//  Test1_Sage_Lawrence
+//
+//  Created by Sage Lawrence on 2024-10-20.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ Test1_Sage_Lawrence/View Controllers/WatchListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WatchListViewController.swift
+//  Test1_Sage_Lawrence
+//
+//  Created by Sage Lawrence on 2024-10-20.
+//
+
+import UIKit
+
+class WatchListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
