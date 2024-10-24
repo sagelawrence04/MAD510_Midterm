82dc41fe48311d74ec241f6a3874e7e49141c1f5
Sage Lawrence <w0811501@myscc.ca>
2024-10-24 15:15:38 -0400

    WatchList Loads Data & VideoClip plays

---
+++ Test1_Sage_Lawrence/View Controllers/FavClipViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FavClipViewController.swift
+//  Test1_Sage_Lawrence
+//
+//  Created by Sage Lawrence on 2024-10-24.
+//
+
+import UIKit
+
+class FavClipViewController: AVPlayerViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
